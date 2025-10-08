1e9715d969e2b0fabcce5eccf3ae62fe182466e7
AyumuRAI <rai.ayumucueva@gmail.com>
2025-10-08 17:06:10 +0800

    Fix: missing activity indicator

--- screens/LoansScreen.js
+++ screens/LoansScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, ScrollView, TouchableOpacity, Alert, RefreshControl } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity, Alert, RefreshControl, ActivityIndicator } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Ionicons } from '@expo/vector-icons';
 
